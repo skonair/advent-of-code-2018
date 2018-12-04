eb4d6570dadcda2053efa4dc6395a2328c86510c
Sascha Koch <sascha.koch@web.de>
2018-12-04 17:10:48 +0100

    some refactoring

--- src/advent_of_code_2018/day4.clj
+++ src/advent_of_code_2018/day4.clj
@@ -21,33 +21,37 @@
             (assoc akk guard (concat (get akk guard) (range begin (:minute l))))
             akk))))))
 
-(defn most-frequent [items]
+(defn get-most-frequent-vals [items]
     (->> items
           frequencies
           (sort-by val)
           reverse
           first))
 
+(defn frequency-stats [st] 
+  (let [mf (get-most-frequent-vals (second st))]
+    (vector
+      (count (second st))
+      (second mf)
+      (first mf)
+      (Integer/parseInt (first st)))))
+
 (defn black-magic-fuckery [fkt lines]
-  (apply * (drop 2
-  (first
-    (reverse
-  (sort-by fkt
-    (map 
-      (fn [st] (let [mf (most-frequent (second st))]
-        (vector 
-          (count (second st))
-          (second mf)
-          (first mf)
-          (Integer/parseInt (first st)))))
-      (sleeptime lines))))))))
+  (->> lines
+    sleeptime
+    (map frequency-stats)
+    (sort-by fkt)
+    reverse
+    first
+    (drop 2)
+    (apply *)))
 
 (defn- parse-line [line]
   (let [[r yyyy m dd hh mm msg] (re-find #".(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2})..(.*)" line)]
     {:minute (Integer/parseInt mm) :msg msg}))
 
 (def slurp-list 
-  (map parse-line (sort (clojure.string/split-lines (slurp "resources/day4.txt")))))
+  (map parse-line (sort (str/split-lines (slurp "resources/day4.txt")))))
 
 (println "Day 4, Part 1: " (black-magic-fuckery first slurp-list))
 (println "Day 4, Part 2: " (black-magic-fuckery second slurp-list))
