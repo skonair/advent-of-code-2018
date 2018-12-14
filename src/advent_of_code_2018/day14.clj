92892f370071898ee2c40e876071508449754172
Sascha Koch <sascha.koch@web.de>
2018-12-14 19:07:34 +0100

    day14

--- src/advent_of_code_2018/day14.clj
+++ src/advent_of_code_2018/day14.clj
@@ -1,8 +1,56 @@
 (ns advent-of-code-2018.day14)
 
-(comment def slurp-list 
-  (clojure.string/split-lines (slurp "resources/day14.txt")))
+(defn n2dl [n]
+  (loop [i n
+         akk '()]
+    (if (< i 10) (conj akk i)
+      (recur (quot i 10) (conj akk (mod i 10))))))
 
-(println "Day 14, Part 1: " ) 
-(println "Day 14, Part 2: " )
+(defn new-recipe [recipes p1 p2]
+  (let [s (+ (nth recipes p1) (nth recipes p2))]
+    (n2dl s)))
+
+(defn round [recipes p1 p2]
+  (let [m1 (inc (nth recipes p1))
+        m2 (inc (nth recipes p2))
+        new-recipes (into recipes (new-recipe recipes p1 p2))
+        rlen (count new-recipes)]
+    [new-recipes (mod (+ p1 m1) rlen) (mod (+ p2 m2) rlen)]))
+
+(defn rounds [recipes n]
+  (loop [rcp recipes
+         p1 0
+         p2 1] 
+    (if (< n (count rcp))
+      [rcp p1 p2]
+      (let [[nxt-rcp nxt-p1 nxt-p2] (round rcp p1 p2)]
+        (recur nxt-rcp nxt-p1 nxt-p2)))))
+
+(defn score [recipes n]
+  (let [[rcp p1 p2] (rounds recipes (+ n 10))]
+    (clojure.string/join "" (take 10 (drop n rcp)))))
+
+(defn ends-with? [recipes pattern]
+  (if (<= (count recipes) (count pattern))
+    false
+    (let [lr (count recipes) 
+          lp (count pattern)
+          s1 (- lr lp) ]
+    (or
+      (= pattern (subvec recipes s1))
+      (= pattern (subvec recipes (dec s1) (dec lr)))))))
+
+(defn prefix-length [recipes pattern]
+  (loop [rcp recipes
+         p1 0
+         p2 1] 
+    (if (ends-with? rcp pattern)
+      (let [d (- (count rcp) (count pattern))]
+        (if (= pattern (subvec rcp d)) d (dec d))) 
+      (let [[nxt-rcp nxt-p1 nxt-p2] (round rcp p1 p2)]
+        (recur nxt-rcp nxt-p1 nxt-p2)))))
+
+(println "Day 14, Part 1: " (score [3 7] 5))
+(println "Day 14, Part 2: " (prefix-length [3 7] [5 1 3 4 0 1]))
+
 
--- test/advent_of_code_2018/day14_test.clj
+++ test/advent_of_code_2018/day14_test.clj
@@ -2,6 +2,18 @@
   (:require [clojure.test :refer :all]
             [advent-of-code-2018.day14 :refer :all]))
 
-(deftest ???-test []
-         (testing "" (is (=  ())))
-         )
+(deftest part1-test []
+         (testing "After 9 recipes, the scores of the next ten would be 5158916779." (is (= "5158916779" (score [3 7] 9))))
+         (testing "After 5 recipes, the scores of the next ten would be 0124515891." (is (= "0124515891" (score [3 7] 5))))
+         (testing "After 18 recipes, the scores of the next ten would be 9251071085" (is (= "9251071085" (score [3 7] 18))))
+         (testing "After 2018 recipes, the scores of the next ten would be 5941429882" (is (= "5941429882" (score [3 7] 2018)))))
+
+(deftest part2-test []
+         (testing "51589 first appears after 9 recipes." (is (= 9 (prefix-length [3 7] [5 1 5 8 9]))))
+         (testing "01245 first appears after 5 recipes." (is (= 5 (prefix-length [3 7] [0 1 2 4 5]))))
+         (testing "92510 first appears after 18 recipes." (is (= 18 (prefix-length [3 7] [9 2 5 1 0]))))
+         (testing "59414 first appears after 2018 recipes." (is (= 2018 (prefix-length [3 7] [5 9 4 1 4])))))
+
+
+
+
